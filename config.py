516fbde5b02d53fda20d636bfaece8c3e7ba4756
ᴛᴇᴀᴍ ᴀʟɪsʜᴀ <116377135+Team-Alisha@users.noreply.github.com>
2023-01-03 17:32:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Team-Alisha/FallenMusic",
+    "https://github.com/Team-Alisha/Alisha",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
